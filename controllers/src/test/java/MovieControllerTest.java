e9393b63a416548dd1d4ae5c0ea56e8fe7c9b5d2
Maknoob <imak90@gmail.com>
2024-06-15 13:41:47 +0200

    added MovieControllerTest.java

---
+++ controllers/src/test/java/MovieControllerTest.java
@@ -0,0 +1,44 @@
+
+import de.codingmak.learning.exceptions.InvalidPartException;
+import de.codingmak.learning.exceptions.MovieNotFoundException;
+import de.codingmak.learning.models.Movie;
+import de.codingmak.learning.services.MovieService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class MovieControllerTest {
+
+    private MovieService movieService;
+
+    @BeforeEach
+    public void setUp() {
+    movieService = new MovieService();
+    }
+
+    @Test
+    public void testGetMovieByPart_ValidPart() {
+        List<Movie> movies = movieService.getMovieByPart("1");
+        assertEquals(1, movies.size());
+        assertEquals("Harry Potter and the Philosopher's Stone", movies.get(0).getName());
+    }
+
+    @Test
+    public void testGetMovieByPart_InvalidPartFormat() {
+        assertThrows(InvalidPartException.class, () -> movieService.getMovieByPart("invalid"));
+    }
+
+    @Test
+    public void testGetMovieByPart_PartNotFound() {
+        assertThrows(MovieNotFoundException.class, () -> movieService.getMovieByPart("9"));
+    }
+}
