6f2ee7b90ad879d75dc4ffccac468e7ce986d0b1
Maknoob <imak90@gmail.com>
2024-06-15 15:08:24 +0200

    added exceptions handling for spell controller

--- controllers/src/main/java/de/codingmak/learning/controllers/SpellController.java
+++ controllers/src/main/java/de/codingmak/learning/controllers/SpellController.java
@@ -1,12 +1,11 @@
 package de.codingmak.learning.controllers;
 
+import de.codingmak.learning.exceptions.SpellNotFoundException;
 import de.codingmak.learning.models.Spell;
 import de.codingmak.learning.services.SpellService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,5 +37,11 @@ public class SpellController {
     @GetMapping(params = "unforgiven")
     public List<Spell> getUnforgivenSpells(@RequestParam(name = "unforgiven") boolean unforgiven) {
         return spellService.getUnforgivenSpells(unforgiven);
+    }
+
+    @ExceptionHandler(SpellNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleSpellNotFoundException(SpellNotFoundException e) {
+        return e.getMessage();
     }
 }
