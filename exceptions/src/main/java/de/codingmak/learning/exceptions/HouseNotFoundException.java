14ade7a539c93495050510f26444fd3db92ae060
Maknoob <imak90@gmail.com>
2024-06-16 14:28:25 +0200

    deleted for a global NotFoundException.java

--- exceptions/src/main/java/de/codingmak/learning/exceptions/HouseNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package de.codingmak.learning.exceptions;
-
-public class HouseNotFoundException extends RuntimeException {
-
-    public HouseNotFoundException(String message) {
-        super(message);
-    }
-}
--- exceptions/src/main/java/de/codingmak/learning/exceptions/SpellNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package de.codingmak.learning.exceptions;
-
-public class SpellNotFoundException extends RuntimeException{
-
-    public SpellNotFoundException(String message) {
-        super(message);
-    }
-}
